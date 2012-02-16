c050245cc97c929089085fd2694e3cc225816505
xu han <xhan87@gmail.com>
2012-02-16 18:07:19 +0800

    added first class

---
+++ src/AppleJam.h
@@ -0,0 +1,13 @@
+//
+//  AppleJam.h
+//  LessDJ
+//
+//  Created by xu xhan on 2/16/12.
+//  Copyright (c) 2012 xu han. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppleJam : NSObject
+
+@end
---
+++ src/AppleJam.m
@@ -0,0 +1,13 @@
+//
+//  AppleJam.m
+//  LessDJ
+//
+//  Created by xu xhan on 2/16/12.
+//  Copyright (c) 2012 xu han. All rights reserved.
+//
+
+#import "AppleJam.h"
+
+@implementation AppleJam
+
+@end
